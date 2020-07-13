d0a447887616a2a70e019cf16a484160c4a359cd
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-07-13 15:35:42 +0300

    включил обратно express

---
+++ frontend/pages/auth.js
@@ -0,0 +1,15 @@
+//React
+import React from "react";
+
+//App
+import Layout from "../components/Layout";
+
+const AuthPage = () => {
+    return (
+        <Layout>
+
+        </Layout>
+    )
+}
+
+export default AuthPage
